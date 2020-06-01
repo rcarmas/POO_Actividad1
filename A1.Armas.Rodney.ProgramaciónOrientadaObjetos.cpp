1f6351cd5a1d229c71843dfc749253555c2a4f88
jacquelinegavilanes <65547052+jacquelinegavilanes@users.noreply.github.com>
2020-05-31 19:27:26 -0500

    Update A1.Armas.Rodney.ProgramaciónOrientadaObjetos.cpp

--- A1.Armas.Rodney.ProgramaciónOrientadaObjetos.cpp
+++ A1.Armas.Rodney.ProgramaciónOrientadaObjetos.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main() {
 cout <<"Hola Mundo"<< endl;
+  char mensaje[10000];
+    cout<<"Ingrese mensaje que desea mostrar"<<endl;
+    cin.getline(mensaje,40);
+    cout<<"El mensaje es"<< endl;
+    cout<<mensaje<< endl;
 system ("Pause");
 return 0;
 }
